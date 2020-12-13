278305583932cb82c11a4d1fc6164a82c8ff3ad3
Alida <hjelljordalida@gmail.com>
2020-12-13 14:21:05 +0100

    Version controll (clearing ut mistakes)

--- Assignment 3/Prime.java
+++ Assignment 3/Prime.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Prime {
     public static boolean isPrime(int n){
         for(int i = 2; i < n; i++){
@@ -5,31 +7,24 @@ public class Prime {
         }
         return true;
     }
-    public static void factorize(int n) {
-        for(int i = 2; i < n; i++){
-            if(n % i == 0) System.out.println(n + " / " + i + " = " + (n/i));
-        }
-    }
     
     public static void main(String[] args) {
-        // Scanner input = new Scanner(System.in);
-        // boolean carryon = true; 
-        // while(carryon){
-        //     System.out.println("Enter number (Exit with \"esc\"): ");
-        //     String in = input.nextLine();
-        //     if(in.equals("esc")){
-        //         carryon = false;
-        //         continue;
-        //     }
-        //     int n = Integer.parseInt(in);
-        //     if(isPrime(n)){ 
-        //         System.out.println(n + " is prime.");
-        //     }else{
-        //         System.out.println(n + " is not prime.");
-        //     }
-        // }
-        // input.close();
-        factorize(362);
-        factorize(209);
+        Scanner input = new Scanner(System.in);
+        boolean carryon = true; 
+        while(carryon){
+            System.out.println("Enter number (Exit with \"esc\"): ");
+            String in = input.nextLine();
+            if(in.equals("esc")){
+                carryon = false;
+                continue;
+            }
+            int n = Integer.parseInt(in);
+            if(isPrime(n)){ 
+                System.out.println(n + " is prime.");
+            }else{
+                System.out.println(n + " is not prime.");
+            }
+        }
+        input.close();
     }
 }
